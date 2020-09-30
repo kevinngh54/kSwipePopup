0e6d90afbe29e0159e4089e5eb3884407e6a2701
khoinh <khoinh@ttc-solutions.com>
2020-09-30 21:24:05 +0700

    Initial Commit

---
+++ KSwipePopup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KSwipePopup
+//
+//  Created by NHK on 9/30/20.
+//  Copyright © 2020 NHK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
